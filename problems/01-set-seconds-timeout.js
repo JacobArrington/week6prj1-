8e0af0bc468d2896e1f59b92686bc46ccbe81944
Jacob Arrington <Jacobsarrington@gmail.com>
2022-08-23 18:59:05 -0400

    probs 1-2

--- problems/01-set-seconds-timeout.js
+++ problems/01-set-seconds-timeout.js
@@ -24,8 +24,16 @@ setSecondsTimeout(function () {
 ***********************************************************************/
 
 function setSecondsTimeout(cb, delayInSeconds) {
-  // Your code here
+  let delaymili = delayInSeconds * 1000
+  setTimeout(cb, delaymili)
 }
+setSecondsTimeout(function () {
+  console.log('hello');
+}, 1); // should print 'hello' after 1000 milliseconds
+
+setSecondsTimeout(function () {
+  console.log('world');
+}, 1.4); // should print 'world' after 1400 milliseconds
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
